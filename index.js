524426be219fe9d669b2d4267329e5610244bbe5
James <tikx.batera@gmail.com>
2019-02-08 16:33:18 -0200

    Users can see survey list.

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
@@ -23,6 +25,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets like our main.js or main.css
